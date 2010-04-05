a05b8e835289c7672e3f2505094bd1247bced021
Dark <dark_rulerz@hotmail.com>
2010-04-05 15:21:38 +0200

    Fix rogue talent Killing Spree
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5058,15 +5058,19 @@ void Aura::HandleAuraPeriodicDummy(bool apply, bool Real)
     {
         case SPELLFAMILY_ROGUE:
         {
-            if(!apply)
+            switch(spell->Id)
             {
-                switch(spell->Id)
-                {
-                    // Master of Subtlety
-                    case 31666: m_target->RemoveAurasDueToSpell(31665); break;
-                    // Overkill
-                    case 58428: m_target->RemoveAurasDueToSpell(58427); break;
-                }
+                // Master of Subtlety
+                case 31666: if(!apply) m_target->RemoveAurasDueToSpell(31665); break;
+                // Overkill
+                case 58428:	if(!apply) m_target->RemoveAurasDueToSpell(58427); break;
+                // Killing Spree
+                case 51690:
+                    if(apply)
+                        m_target->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                    else
+                        m_target->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                    break;
             }
             break;
         }
@@ -5074,14 +5078,13 @@ void Aura::HandleAuraPeriodicDummy(bool apply, bool Real)
         {
             switch (spell->Id)
             {
+                //Demonic Circle
                 case 48018:
                     if (apply)
-                        SendFakeAuraUpdate(62388,false);
-                    else
                     {
-                        m_target->RemoveGameObject(spell->Id,true);
-                        SendFakeAuraUpdate(62388,true);
-                    }
+                        if (m_target->GetGameObject(spell->Id))
+                            m_target->RemoveGameObject(spell->Id,true);
+                    }                         
                 break;
             }
         }
@@ -8687,7 +8690,8 @@ void Aura::HandleAllowOnlyAbility(bool apply, bool Real)
     if(!Real)
        return;
 
-    if(apply)
+    // aura should affect only abilities
+    /*if(apply)
     {
        m_target->setAttackTimer(BASE_ATTACK,m_duration);
        m_target->setAttackTimer(RANGED_ATTACK,m_duration);
@@ -8697,7 +8701,7 @@ void Aura::HandleAllowOnlyAbility(bool apply, bool Real)
     {
        m_target->resetAttackTimer(BASE_ATTACK);
        m_target->resetAttackTimer(RANGED_ATTACK);
-       m_target->resetAttackTimer(OFF_ATTACK);
+       m_target->resetAttackTimer(OFF_ATTACK);*/
     }
 
     m_target->UpdateDamagePhysical(BASE_ATTACK);
