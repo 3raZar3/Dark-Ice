5280731c39ea762dab0facef0346451e6d6c528b
Dark <dark_rulerz@hotmail.com>
2010-04-05 15:19:25 +0200

    In Battleground stats should be in Honor gained fields also honors from killing a player
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -647,7 +647,8 @@ void BattleGround::RewardHonorToTeam(uint32 Honor, uint32 TeamID)
         if(!team) team = plr->GetTeam();
 
         if (team == TeamID)
-            UpdatePlayerScore(plr, SCORE_BONUS_HONOR, Honor);
+            if (plr->RewardHonor(NULL, 1, (float)Honor))
+                UpdatePlayerScore(plr, SCORE_BONUS_HONOR, Honor);
     }
 }
 
@@ -1389,11 +1390,7 @@ void BattleGround::UpdatePlayerScore(Player *Source, uint32 type, uint32 value)
         case SCORE_BONUS_HONOR:                             // Honor bonus
             // do not add honor in arenas
             if (isBattleGround())
-            {
-                // reward honor instantly
-                if (Source->RewardHonor(NULL, 1, (float)value))
-                    itr->second->BonusHonor += value;
-            }
+                itr->second->BonusHonor += value;
             break;
             //used only in EY, but in MSG_PVP_LOG_DATA opcode
         case SCORE_DAMAGE_DONE:                             // Damage Done
