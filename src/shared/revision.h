bef4d2d2112ba05630a76bc5f1c670b02bc9b0d1
Dark <dark_rulerz@hotmail.com>
2010-04-11 15:56:49 +0200

    Upload missing files (Damn you GIT for not seeing them!)
    Signed-off-by: Dark <dark_rulerz@hotmail.com>

---
+++ src/shared/revision.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "17a9876d9fea2d66a6ea1e9e0b135eca94801b36"
+ #define REVISION_DATE "*"
+ #define REVISION_TIME "*"
+#endif // __REVISION_H__
