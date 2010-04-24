c51e7ad1cd36c58d31d22dc1acd77f71593ebabc
Skinlayers <skinrelays@yahoo.com>
2010-04-24 16:00:03 -0700

    Fixed SD2 compile error

--- src/bindings/ScriptDev2/scripts/northrend/naxxramas/naxxramas.h
+++ src/bindings/ScriptDev2/scripts/northrend/naxxramas/naxxramas.h
@@ -129,7 +129,7 @@ class MANGOS_DLL_DECL instance_naxxramas : public ScriptedInstance
         uint32 GetData(uint32 uiType);
         uint64 GetData64(uint32 uiData);
 
-        void instance_naxxramas::SwitchDoor(uint32 uiData, uint64 doorGUID);
+        void SwitchDoor(uint32 uiData, uint64 doorGUID);
 		
         const char* Save() { return strInstData.c_str(); }
         void Load(const char* chrIn);
