6e1ea2ac5fa5f5b91d6a8fc5d6ecb0f7150c6d26
3raZar3 <3raZar3@clanice.com>
2010-04-25 12:34:29 -0400

    Allow to cast AoE spells by charmed units no matter if they are positive or negative (targets checks are handled elswhere) -ty 2 przemratajczak

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5343,7 +5343,21 @@ SpellCastResult Spell::CheckPetCast(Unit* target)
                 return SPELL_FAILED_BAD_TARGETS;
 
 			if(!IsValidSingleTargetSpell(_target))
-				return SPELL_FAILED_BAD_TARGETS;             
+				return SPELL_FAILED_BAD_TARGETS;
+            else
+            {
+                bool duelvsplayertar = false;
+                for(int j = 0; j < MAX_EFFECT_INDEX; ++j)
+                {
+                                                            //TARGET_DUELVSPLAYER is positive AND negative
+                    duelvsplayertar |= (m_spellInfo->EffectImplicitTargetA[j] == TARGET_DUELVSPLAYER);
+                }
+                if(m_caster->IsFriendlyTo(target) && !duelvsplayertar && !IsDispelSpell(m_spellInfo) && 
+                    !(m_caster->isCharmed() && IsAreaOfEffectSpell(m_spellInfo)))
+                {
+                    return SPELL_FAILED_BAD_TARGETS;
+                }
+            }
         }
                                                             //cooldown
         if(((Creature*)m_caster)->HasSpellCooldown(m_spellInfo->Id))
