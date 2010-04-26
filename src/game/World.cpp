fa56c13481183fb17ea9e8beb0a4a2d1548b2147
3raZar3 <3raZar3@clanice.com>
2010-04-26 14:40:31 -0400

    A bit more cleanup in OPvP logs -Feanordev

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1373,6 +1373,10 @@ void World::SetInitialWorldSettings()
     sBattleGroundMgr.CreateInitialBattleGrounds();
     sBattleGroundMgr.InitAutomaticArenaPointDistribution();
 
+    ///- Initialize outdoor pvp
+    sLog.outString( "Starting Outdoor PvP System" );
+    sOutdoorPvPMgr.InitOutdoorPvP();
+
     //Not sure if this can be moved up in the sequence (with static data loading) as it uses MapManager
     sLog.outString( "Loading Transports..." );
     sMapMgr.LoadTransports();
