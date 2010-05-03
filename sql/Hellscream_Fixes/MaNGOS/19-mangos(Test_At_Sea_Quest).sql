ef551229d6de870546ccd9e4e8246881db75cc5a
GMHAZE <iceis2@hotmail.com>
2010-05-02 23:03:54 -0400

    added quest test at sea

---
+++ sql/Hellscream_Fixes/MaNGOS/19-mangos(Test_At_Sea_Quest).sql
@@ -0,0 +1,2 @@
+-- Test at Sea
+UPDATE `creature_template` SET `ScriptName`='npc_apothecary_lysander' WHERE (`entry`='24126');
---
+++ sql/Hellscream_Fixes/SD2/10-scriptdev2(Test_At_Sea_Quest).sql
@@ -0,0 +1,14 @@
+-- Test at Sea
+REPLACE INTO script_texts
+   (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+   (-1050020, 'Reports from the test on the enemy fleet have come in, sir!', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050021, 'Excellent. What\'s the lowdown? Do we have a bodycount?', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050022, 'Well... the good news is that we forced the reinforcements to turn back.', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050023, 'Well, spit it out! What\'s the bad news?', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050024, 'The plague was not quite... fatal. However, reports of harmful effects include nausea, loss of equilibrium and severe stomach pain.', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050025, 'Severe stomach pain?', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050026, 'SEVERE STOMACH PAIN?', '', '', '', '', '', '', '', '', 0, 1, 0, 0, ''),
+   (-1050027, 'We are the Royal Apothecary Society! Inducing belly aches is not part of our job description!', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050028, 'Am I surrounded by amateurs? I want a full analysis of the new strain by tomorrow morning! Get on it, Ravien!', '', '', '', '', '', '', '', '', 0, 0, 0, 0, ''),
+   (-1050029, 'Yes, sir!', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '');
