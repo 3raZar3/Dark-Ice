e128f7b9946ec80aa93086dc6d2cbb1ede8ab36d
3raZar3 <3raZar3@clanice.com>
2010-05-05 10:17:25 -0400

    Fixed auras rewarded in quest The Crone`s Bargain, also restored propper faction templates -przemratajczak
    Signed-off-by: 3raZar3 <3raZar3@clanice.com>

--- DB_Updates/a441_Q_YoullNeedaBear.sql
+++
@@ -1,4 +0,0 @@
--- Quest :: You'll Need a Bear - 12972
--- wrong factions for alliance nad horde
-UPDATE `creature_template` SET `faction_A` = '35',
-`faction_H` = '35' WHERE `entry` =29592 LIMIT 1 ;
