e5e4bab69f0ad85f19b349f806118cf46f8a60fe
GMHAZE <iceis2@hotmail.com>
2010-05-05 23:26:11 -0400

    added achievement support from sd2

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -22752,6 +22752,12 @@ Object* Player::GetObjectByTypeMask(ObjectGuid guid, TypeMask typemask)
     return NULL;
 }
 
+
+void Player::CompletedAchievement(AchievementEntry const* entry)
+{
+  GetAchievementMgr().CompletedAchievement(entry);
+}
+
 void Player::SetRestType( RestType n_r_type, uint32 areaTriggerId /*= 0*/)
 {
     rest_type = n_r_type;
