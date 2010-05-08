5896c1a6148b2a0cc1b5ea07286f2cb263a6a530
Dark <dark_rulerz@hotmail.com>
2010-05-08 14:24:16 +0200

    Fix Gag order (thx MrLama)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5888,6 +5888,12 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 triggered_spell_id = 26654;
                 break;
             }
+            case 12311: // Gag Order rank 1 
+            case 12958: // Gag Order rank 2 
+            { 
+                triggered_spell_id = 18498; 
+                break; 
+            }
             break;
         }
         case SPELLFAMILY_WARLOCK:
