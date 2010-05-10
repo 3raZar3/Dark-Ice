81d0df4c1449b25b7f65f8029d7590ef4a6df93b
Gimli <admin@isengard.dk>
2010-05-10 11:54:30 +0200

    modified:   src/game/World.h

--- src/game/World.h
+++ src/game/World.h
@@ -699,6 +699,9 @@ class World
         bool m_MvAnticheatWaterCheck;
         bool m_MvAnticheatFlyCheck;
         bool m_MvAnticheatMountainCheck;
+        bool m_MvAnticheatJumpCheck;
+        bool m_MvAnticheatTeleportCheck;
+        bool m_MvAnticheatTeleport2PlaneCehck;
 
         // CLI command holder to be thread safe
         ACE_Based::LockedQueue<CliCommandHolder*,ACE_Thread_Mutex> cliCmdQueue;
