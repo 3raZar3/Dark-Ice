223061b6b9afeb8a09a837925bd946e40fe80dfe
selector <limurchick@gmail.com>
2010-05-13 17:07:17 +0300

    RFC records based on warlord123 tables

---
+++ addition/RFC/RFC_achievment.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 5.1.42-50.5.0.3.8 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('33','1358','Complete 130 quests in Borean Tundra','Complete 150 quests in Borean Tundra');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('34','1356','Complete 130 quests in Howling Fjord','Complete 105 quests in Howling Fjord');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('35','1359','Complete 115 quests in Dragonblight','Complete 130 quests in Dragonblight');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('37','1357','Complete 85 quests in Grizzly Hills','Complete 75 quests in Grizzly Hills');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('203','1251','In a single Warsong Gulch battle, kill 2 flag carriers before they leave the Silverwing Flag Room','In a single Warsong Gulch battle, kill 2 flag carriers before they leave the Warsong Flag Room');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('220','873','Win Alterac Valley without losing a tower or captain. You must also control all of the Horde\'s towers','Win Alterac Valley without losing a tower or captain. You must also control all of the Alliance\'s towers');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('433','443','Earned the title, Grand Marshal','Earned the title, High Warlord');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('434','445','Earned the title, Field Marshal','Earned the title, Warlord');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('435','444','Earned the title, Commander','Earned the title, Lieutenant General');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('436','447','Earned the title, Lieutenant Commander','Earned the title, Champion');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('437','448','Earned the title, Knight-Champion','Earned the title, Centurion');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('438','469','Earned the title, Knight-Captain','Earned the title, Legionnaire');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('439','451','Earned the title, Knight','Earned the title, Stone Guard');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('440','452','Earned the title, Sergeant Major','Earned the title, First Sergeant');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('441','450','Earned the title, Master Sergeant','Earned the title, Senior Sergeant');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('442','454','Earned the title, Private','Earned the title, Scout');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('470','468','Earned the title, Corporal','Earned the title, Grunt');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('472','449','Earned the title, Knight-Lieutenant','Earned the title, Blood Guard');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('473','446','Earned the title, Marshal','Earned the title, General');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('604','603','Kill 5 Horde players in each of the cities listed below','Kill 5 Alliance players in each of the cities listed below');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('610','615','Kill Thrall','Kill King Varian Wrynn');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('611','616','Kill Cairne Bloodhoof','Kill King Magni Bronzebeard');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('612','617','Kill Lady Sylvanas Windrunner','Kill High Priestess Tyrande Whisperwind');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('613','618','Kill Lor\'themar Theron','Kill Prophet Velen');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('614','619','Slay the leaders of the Horde','Slay the leaders of the Alliance');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('701','700','Obtain an Insignia or Medallion of the Alliance','Obtain an Insignia or Medallion of the Horde');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('707','706','Obtain a Stormpike Battle Charger','Obtain a Frostwolf Howler');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('709','708','Gain exalted reputation with the Stormpike Guard','Gain exalted reputation with the Frostwolf Clan');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('711','710','Gain exalted reputation with The League of Arathor','Gain exalted reputation with The Forsaken Defilers');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('713','712','Gain exalted reputation with the Silverwing Sentinels','Gain exalted reputation with the Warsong Outriders');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('899','901','Earn exalted status with the Kurenai','Earn exalted status with The Mag\'har');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('942','943','Raise your reputation level from unfriendly to exalted with Timbermaw Hold, Sporeggar and the Kurenai','Raise your reputation level from unfriendly to exalted with Timbermaw Hold, Sporeggar and The Mag\'har');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1012','1011','Gain exalted reputation with the Alliance Vanguard','Gain exalted reputation with the Horde Expedition');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1028','1031','Desecrate the Horde\'s bonfires in Eastern Kingdoms','Desecrate the Alliance\'s bonfires in Eastern Kingdoms');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1029','1032','Desecrate the Horde\'s bonfires in Kalimdor','Desecrate the Alliance\'s bonfires in Kalimdor');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1030','1033','Desecrate the Horde\'s bonfires in Outland','Desecrate the Alliance\'s bonfires in Outland');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1034','1036','Complete the Flame Warden of Eastern Kingdoms, Kalimdor and Outland achievements','Complete the Flame Keeper of Eastern Kingdoms, Kalimdor and Outland achievements');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1040','1041','Ruin Hallow\'s End for the Horde by completing Sergeant Hartman\'s quests which involve crashing the wickerman festival and cleaning up the stinkbombs from Southshore','Ruin Hallow\'s End for the Alliance by completing Darkcaller Yanka\'s quests which involve going to Southshore, ruining the kegs with rotten eggs and tossing stinkbombs into the town');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1151','224','In Alterac Valley, kill 50 enemy players in the Hall of the Stormpike','In Alterac Valley, kill 50 enemy players in the Hall of the Frostwolf');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1189','1271','Complete 80 quests in Hellfire Peninsula','Complete 90 quests in Hellfire Peninsula');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1191','1272','Complete 63 quests in Terokkar Forest','Complete 68 quests in Terokkar Forest');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1192','1273','Complete 75 quests in Nagrand','Complete 87 quests in Nagrand');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1255','259','Throw a snowball at King Magni Bronzebeard during the Feast of Winter Veil','Throw a snowball at Cairne Bloodhoof during the Feast of Winter Veil');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1279','1280','Get completely smashed, put on your best perfume, throw a handful of rose petals on Sraaz and then kiss him. You\'ll regret it in the morning','Get completely smashed, put on your best perfume, throw a handful of rose petals on Jeremiah Payson and then kiss him. You\'ll regret it in the morning');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1676','1677','Complete 700 quests in Eastern Kingdoms','Complete 550 quests in Eastern Kingdoms');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1678','1680','Complete 700 quests in Kalimdor','Complete 685 quests in Kalimdor');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1686','1685','Use Mistletoe on the Alliance Brothers during the Feast of Winter Veil','Use Mistletoe on the Horde Brothers during the Feast of Winter Veil');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('1782','1783','Complete each of the cooking daily quests offered by Katherine Lee in Dalaran','Complete each of the cooking daily quests offered by Awilo Lon\'gomba in Dalaran');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('2421','2420','Hide a Brightly Colored Egg in Stormwind City','Hide a Brightly Colored Egg in Silvermoon City');
+insert into `player_factionchange_achievement` (`alliance_id`, `horde_id`, `CommentA`, `CommentH`) values('3556','3557','Acquire the Spirit of Sharing from a complete Bountiful Table feast at every Alliance capital','Acquire the Spirit of Sharing from a complete Bountiful Table feast at every Horde capital');
---
+++ addition/RFC/RFC_reputations.sql
@@ -0,0 +1,21 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 5.1.42-50.5.0.3.8 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','47','Ironforge','0','530','Darkspear Trolls');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','54','Gnomeregan Exiles','0','81','Thunder Bluff');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','69','Darnassus','0','68','Undercity');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','72','Stormwind','0','76','Orgrimmar');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','509','The League of Arathor','0','510','The Defilers');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','730','Stormpike Guard','0','729','Frostwolf Clan');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','890','Silverwing Sentinels','0','889','Warsong Outriders');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','930','Exodar','0','911','Silvermoon City');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','946','Honor Hold','0','947','Thrallmar');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','978','Kurenai','0','941','The Mag\'har');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','1037','Alliance Vanguard','0','1052','Horde Expedition');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','1050','Valiance Expedition','0','1085','Warsong Offensive');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','1068','Explorers\' League','0','1064','The Taunka');
+insert into `player_factionchange_reputations` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) values('0','1126','The Frostborn','0','1067','The Hand of Vengeance');
