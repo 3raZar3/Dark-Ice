8c09a48acacf88ac41a86c233b3adc7fc8caa45b
GMHAZE <iceis2@hotmail.com>
2010-05-21 02:06:41 -0400

    Some PVP achievements, thx to Hemi.
    and thx to Wowka321
    
    Conflicts:
    
    	src/game/AchievementMgr.cpp
    	src/game/DBCStructure.h
    
    ::::::pvp achievements a few::::::::

--- src/game/AchievementMgr.cpp
+++ src/game/AchievementMgr.cpp
@@ -100,6 +100,8 @@ bool AchievementCriteriaRequirement::IsValid(AchievementCriteriaEntry const* cri
         case ACHIEVEMENT_CRITERIA_TYPE_BE_SPELL_TARGET2:
             break;
         default:
+            if (requirementType == ACHIEVEMENT_CRITERIA_REQUIRE_HOLIDAY)
+            break;
             sLog.outErrorDb( "Table `achievement_criteria_requirement` have data for not supported criteria type (Entry: %u Type: %u), ignore.", criteria->ID, criteria->requiredType);
             return false;
     }
@@ -799,28 +801,30 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     // some hardcoded requirements
                     switch(achievementCriteria->referredAchievement)
                     {
-                        case 214:              // EY, win under 6 minutes
-                        case 226:              // AV, win under 6 minutes
-                        case 159:              // AB, win under 6 minutes
+                        case 214:							// EY, win under 6 minutes
+                        case 226:							// AV, win under 6 minutes
+                        case 159:							// AB, win under 6 minutes
                         {
                             // set 8 minutes because there is 2 minutes long preparation
                             if(GetPlayer()->GetBattleGround()->GetStartTime() > (8 * MINUTE * IN_MILLISECONDS))
-                                continue;     
+                                continue;
+                            
                             break;
                         }
-                        case 201:              // WS, win under 7 minutes
+                        case 201:							// WS, win under 7 minutes
                         {
                             // set 9 minutes because there is 2 minutes long preparation
                             if(GetPlayer()->GetBattleGround()->GetStartTime() > (9 * MINUTE * IN_MILLISECONDS))
                                 continue;
+                            
                             break;
                         }
                         default:
                         {
-                            // those requirements couldn't be found in the dbc
-                            AchievementCriteriaRequirementSet const* data = sAchievementMgr.GetCriteriaRequirementSet(achievementCriteria);
-                            if (!data || !data->Meets(GetPlayer(),unit))
-                                continue;
+                    // those requirements couldn't be found in the dbc
+                    AchievementCriteriaRequirementSet const* data = sAchievementMgr.GetCriteriaRequirementSet(achievementCriteria);
+                    if (!data || !data->Meets(GetPlayer(),unit))
+                        continue;
                             break;
                         }
                     }
@@ -1182,6 +1186,12 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                 // AchievementMgr::UpdateAchievementCriteria might also be called on login - skip in this case
                 if(!miscvalue1)
                     continue;
+                if(AchievementCriteriaRequirementSet const* data = sAchievementMgr.GetCriteriaRequirementSet(achievementCriteria))
+                {
+                    if(!data->Meets(GetPlayer(),unit))
+                        continue;
+                }
+
                 if(achievementCriteria->use_item.itemID != miscvalue1)
                     continue;
                 SetCriteriaProgress(achievementCriteria, 1, PROGRESS_ACCUMULATE);
@@ -1480,7 +1490,7 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     }
                     case 233:					// Bloodthirsty Berserker
                     {
-                        if(bg->GetTypeID() != BATTLEGROUND_EY)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_EY)
                             continue;
                         if(!GetPlayer()->HasAura(23505))
                             continue;
@@ -1510,7 +1520,7 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                 {
                     case 42:							// WS, capture a flag
                     {
-                        if(bg->GetTypeID() != BATTLEGROUND_WS)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_WS)
                             continue;
 
                         if(miscvalue2 == 1)
@@ -1519,13 +1529,13 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     }
                     case 44:                           // WS, return a flag
                     {
-                        if(bg->GetTypeID() != BATTLEGROUND_WS)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_WS)
                             continue;
                         break;
                     }
                     case 183:							// EY, capture a flag
                     {
-                        if(bg->GetTypeID() != BATTLEGROUND_EY)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_EY)
                             continue;
 
                         switch(achievementCriteria->referredAchievement)
@@ -1547,19 +1557,17 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     }
                     case 122:                           // AB, assault a base
                     {
-                        error_log("ide tedy 1");
-                        if(bg->GetTypeID() != BATTLEGROUND_AB)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_AB)
                             continue;
                         
                         if(miscvalue2 == 1)
                             continue;
-                        error_log("ide tedy 2");
 
                         break;
                     }
                     case 123:                           // AB, defend a base
                     {
-                        if(bg->GetTypeID() != BATTLEGROUND_AB)
+                        if(bg->GetTypeID(true) != BATTLEGROUND_AB)
                             continue;
                         
                         if(miscvalue2 == 0)
@@ -1567,16 +1575,36 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
 
                         break;
                     }
-                    case 61:							// AV, assault a tower
+                    case 61:                            // AV, assault a tower
+                    {                   
+                        if(miscvalue2 != 0)
+                            continue;
+                        break;
+                    }
                     case 63:	                        // AV, take a graveyard
+                    {                   
+                        if(miscvalue2 != 1)
+                            continue;
+                        break;
+                    }
                     case 64:	                        // AV, defend a tower
+                    {                   
+                        if(miscvalue2 != 2)
+                            continue;
+                        break;
+                    }
                     case 65:	                        // AV, defend a graveyard
-                        continue;
+                    {                   
+                        if(miscvalue2 != 3)
+                            continue;
+                        break;
+                    }
 
                 }
                 SetCriteriaProgress(achievementCriteria, miscvalue1, PROGRESS_ACCUMULATE);
                 break;
-            }           
+            }
+
             // std case: not exist in DBC, not triggered in code as result
             case ACHIEVEMENT_CRITERIA_TYPE_HIGHEST_HEALTH:
             case ACHIEVEMENT_CRITERIA_TYPE_HIGHEST_SPELLPOWER:
