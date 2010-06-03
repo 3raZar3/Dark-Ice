2e7b5aeedb454139bd3439092654a07fcfa63cfd
3raZar3 <3raZar3@clanice.com>
2010-06-03 11:55:43 -0400

    Implemented SPELLMOD_ALL_EFFECTS to HandleSchoolAbsorb -reeshack
    Signed-off-by: 3raZar3 <3raZar3@clanice.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -7574,6 +7574,9 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
                     break;
             }
 
+            if(Player* modOwner = caster->GetSpellModOwner())
+                modOwner->ApplySpellMod(m_spellProto->Id, SPELLMOD_ALL_EFFECTS, DoneActualBenefit);
+
             DoneActualBenefit *= caster->CalculateLevelPenalty(GetSpellProto());
 
             m_modifier.m_amount += (int32)DoneActualBenefit;
