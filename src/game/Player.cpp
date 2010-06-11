694dc14d6ab146cdfefe813258b156434fd989a8
3raZar3 <3raZar3@clanice.com>
2010-06-11 19:26:19 -0400

    fixed actionbars GUARANTEED -3raZar3

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -17285,7 +17285,7 @@ void Player::SaveToDB()
         "trans_x, trans_y, trans_z, trans_o, transguid, extra_flags, stable_slots, at_login, zone, "
         "death_expire_time, taxi_path, arenaPoints, totalHonorPoints, todayHonorPoints, yesterdayHonorPoints, totalKills, "
         "todayKills, yesterdayKills, chosenTitle, knownCurrencies, watchedFaction, drunk, health, power1, power2, power3, "
-        "power4, power5, power6, power7, specCount, activeSpec, exploredZones, equipmentCache, ammoId, knownTitles, actionBars, gmlevel) VALUES ("
+        "power4, power5, power6, power7, specCount, activeSpec, exploredZones, equipmentCache, ammoId, knownTitles, actionBars) VALUES ("
         << GetGUIDLow() << ", "
         << GetSession()->GetAccountId() << ", '"
         << sql_name << "', "
@@ -17407,8 +17407,6 @@ void Player::SaveToDB()
     }
     ss << "',";
     ss << uint32(GetByteValue(PLAYER_FIELD_BYTES, 2));
-    ss << uint32(m_activeSpec) << ", ";
-    ss << uint32(m_GMLevel);    
     ss << ")";
 
     CharacterDatabase.Execute( ss.str().c_str() );
