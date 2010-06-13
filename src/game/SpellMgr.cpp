0f44b7036370d33c5d8066705206931d021688e0
GMHAZE <iceis2@hotmail.com>
2010-06-13 02:09:30 -0400

    Allow cast in AreaID 4812 spells with 4522 requirements

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3630,6 +3630,9 @@ SpellCastResult SpellMgr::GetSpellAllowedInLocationError(SpellEntry const *spell
             // Try search in next group
             groupEntry = sAreaGroupStore.LookupEntry(groupEntry->nextGroup);
         }
+		
+		if (spellInfo->AreaGroupId == 723 && area_id == 4812)
+		    found = true;
 
         if (!found)
             return SPELL_FAILED_INCORRECT_AREA;
