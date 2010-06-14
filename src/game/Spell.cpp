992eb833f12cd1893b019230ff28e36cf83c1cae
GMHAZE <iceis2@hotmail.com>
2010-06-14 17:00:44 -0400

    small fix for crashes and some rez spells

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3483,9 +3483,6 @@ void Spell::update(uint32 difftime)
         {
             if(m_timer)
             {
-                if (m_targets.getUnitTarget() && !m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
-                    cancel();
-
                 if(difftime >= m_timer)
                     m_timer = 0;
                 else
