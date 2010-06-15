855eddb7714bad842706b796deeef365269ed0ac
GMHAZE <iceis2@hotmail.com>
2010-06-15 16:13:57 -0400

    icc updates thank you list reset

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "247c3fa95fa3b11d4626a88525fc12902839ad78"
+ #define REVISION_ID "*"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
