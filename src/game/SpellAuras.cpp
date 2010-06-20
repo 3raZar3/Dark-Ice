db97415e9e13de54c6419a8329a05755aedb73bc
GMHAZE <iceis2@hotmail.com>
2010-06-20 03:53:23 -0400

    Implent Warrior T10 Protection 4P Bonus

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -7782,6 +7782,9 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
                         // +75% from spell power
                         DoneActualBenefit = caster->SpellBaseHealingBonusDone(GetSpellSchoolMask(spellProto)) * 0.75f;
                     }
+					case SPELLFAMILY_GENERIC:
+					if (GetId()==70845)
+					    DoneActualBenefit = caster->GetMaxHealth() * 0.2f;	
                     break;
                 default:
                     break;
